0cc165ef91876863b42ffdc24d1162fce81e590d
digitalgoldfisj79 <edwardbozzard@gmail.com>
2026-07-16 20:00:38 +0100

    Add committed shell launcher for S3 evaluation phase 1

---
+++ experiments/terminal_cipher_v0_6/run_v060_s3_phase1.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+set -eu
+pip install -q numba rapidfuzz sentencepiece requests
+cd /tmp/v/experiments/terminal_cipher_v0_6
+printf 'V060_S3_EVAL_GIT_HEAD %s\n' "$(git -C /tmp/v rev-parse HEAD)"
+exec torchrun --standalone --nproc_per_node=1 v060_family_s_neural_postprocess.py one /tmp/v
