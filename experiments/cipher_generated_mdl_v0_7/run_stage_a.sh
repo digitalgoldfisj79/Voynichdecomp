15aa19c62cbfa0f3b6413e85871af51787f2ed2d
digitalgoldfisj79 <edwardbozzard@gmail.com>
2026-07-16 22:10:49 +0100

    Install CPU scientific dependencies in v0.7 launcher

--- experiments/cipher_generated_mdl_v0_7/run_stage_a.sh
+++ experiments/cipher_generated_mdl_v0_7/run_stage_a.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eu
-pip install -q scipy
+pip install -q numpy scipy torch --index-url https://download.pytorch.org/whl/cpu
 cd /tmp/v
 printf 'V070_GIT_HEAD %s\n' "$(git rev-parse HEAD)"
 exec python experiments/cipher_generated_mdl_v0_7/v070_source_transfer_mdl.py "$@"
