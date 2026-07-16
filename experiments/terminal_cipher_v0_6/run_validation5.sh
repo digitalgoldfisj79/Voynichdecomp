a479e40d5ac15b22018f5a9b8ff3c2bc5f6bab7b
digitalgoldfisj79 <edwardbozzard@gmail.com>
2026-07-16 21:36:17 +0100

    Add blind model-selection launcher

---
+++ experiments/terminal_cipher_v0_6/run_validation5.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+set -eu
+pip install -q numpy numba rapidfuzz scikit-learn
+cd /tmp/v
+printf 'V060_MODEL_SELECTION_GIT_HEAD %s\n' "$(git rev-parse HEAD)"
+python experiments/terminal_cipher_v0_6/v060_blind_model_selection.py /tmp/v060_blind_model_selection_result.json
